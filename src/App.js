4dad59f28e96cafa49579e8cbabaf151e7e265ad
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-04-18 13:58:39 -0300

    Login placebo created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
