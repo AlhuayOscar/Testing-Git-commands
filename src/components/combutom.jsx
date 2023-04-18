950abd20fed5f0f17d3a372ac30bb30b6481b0d8
estiven2111 <estiven.a.martinez@gmail.com>
2023-04-18 15:01:26 -0500

    estiven cambio

---
+++ src/components/combutom.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function combutom() {
+  return (
+    <div>
+        <label htmlFor="">boton de prueba</label>
+        <button>prueba boton</button>
+    </div>
+  )
+}
+
+export default combutom
